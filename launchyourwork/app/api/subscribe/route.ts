b8cf4fd90ffec25fab39ca60517cef3332ec5755
jenara-jo <jen.araujo112@gmail.com>
2026-03-25 18:06:16 -0600

    update beehiiv api

--- launchyourwork/app/api/subscribe/route.ts
+++ launchyourwork/app/api/subscribe/route.ts
@@ -3,19 +3,34 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const { email } = await req.json();
 
-  const formData = new URLSearchParams();
-  formData.append("email", email);
-
   const res = await fetch(
-    "https://subscribe-forms.beehiiv.com/82417ecd-7955-4a07-809c-e9ddbc92da40",
+    `https://api.beehiiv.com/v2/publications/${process.env.BEEHIIV_PUB_ID}/subscriptions`,
     {
       method: "POST",
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.BEEHIIV_API_KEY}`,
       },
-      body: formData.toString(),
+      body: JSON.stringify({
+        email: email,
+        reactivate_existing: true,
+        send_welcome_email: false,
+        utm_source: "launch_your_work",
+        utm_medium: "landing_page",
+        double_opt_override: "off",
+        automation_ids: [process.env.BEEHIIV_AUTOMATION_ID],
+      }),
     },
   );
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    return NextResponse.json(
+      { error: "Failed to subscribe", details: data },
+      { status: 500 },
+    );
+  }
 
   return NextResponse.json({ success: true });
 }
